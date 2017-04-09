f99169c9f464ab863fbed2278d09a9e964613c49
Toufiq <burningraven06@gmail.com>
2017-04-09 21:04:20 +0600

    BookPublisherMigration Error Fixed

--- db/migrate/20160909191320_add_publisher_to_books.rb
+++ db/migrate/20160909191320_add_publisher_to_books.rb
@@ -1,6 +1,6 @@
 class AddPublisherToBooks < ActiveRecord::Migration[5.0]
   def change
-  	add_index :books, :publisher_id
+  	# add_index :books, :publisher_id
   	# add_index :books, :id
   end
 end
