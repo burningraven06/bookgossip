fa0177f8b73c91e82c92d1c9f5b7624f3db6b935
Toufiq <burningraven06@gmail.com>
2017-07-07 20:54:10 +0600

    Production SMTP Issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
 	config.action_mailer.default charset: "utf-8"
 	config.action_mailer.delivery_method= :smtp
 	config.action_mailer.perform_deliveries =true
-	config.action_mailer.default_url_options = { :host => "bookgossip.herokuapp.com" }
+	config.action_mailer.default_url_options = { :host => "bookgossip.herokuapp.com", only_path: false }
 
 	config.action_mailer.smtp_settings = {
 		address: ENV['ELASTIC_ADDRESS'],
