59d564f35bb626e42883e5bfff04eb00d060c229
Toufiq <burningraven06@gmail.com>
2017-07-07 20:30:46 +0600

    SMTP Info in Production.rb updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,7 @@ Rails.application.configure do
 	config.active_record.dump_schema_after_migration = false
 
 	config.action_mailer.default charset: "utf-8"
+	config.action_mailer.delivery_method= :smtp
 	config.action_mailer.perform_deliveries =true
 	config.action_mailer.default_url_options = { :host => "bookgossip.herokuapp.com" }
 
