942547f156fa6554169140a6fa2bdca862e480f2
Toufiq <burningraven06@gmail.com>
2017-07-07 21:49:03 +0600

    gmail smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,7 +101,7 @@ Rails.application.configure do
 	config.action_mailer.smtp_settings = {
 		address: ENV['GMAIL_ADDRESS'],
 		port: 587,
-		domain: "bookgossip.herokuapp.com",
+		domain: "heroku.com",
 		user_name: ENV["GMAIL_EMAIL"],
 		password: ENV["GMAIL_PASSWORD"],
 		authentication: "plain",
