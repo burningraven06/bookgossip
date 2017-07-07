03f01dd3cda17e4cde777fc356a677241c464f33
Toufiq <burningraven06@gmail.com>
2017-07-07 22:37:18 +0600

    gmail smtp production problem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 	# }
 
 	config.action_mailer.smtp_settings = {
-		address: ENV['GMAIL_ADDRESS'],
+		address: "smtp.gmail.com",
 		port: 587,
 		user_name: ENV["GMAIL_EMAIL"],
 		password: ENV["GMAIL_PASSWORD"],
